e914994d5a91c5c4fc36fb1458bab55c2dc2942a
Michael Lannin <mlannin23@gmail.com>
2015-06-14 15:07:00 -0700

    Add scrollspy and nav links

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -8,4 +8,45 @@ $(document).ready(function() {
             $(".navbar-main-scroll").removeClass("navbar-main-scroll");
         }
     });
+
+    $('body').scrollspy({ 
+        target: '#navbar-collapse',
+        offset: 64
+    });
+
+    $("#about-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top - 64
+        }, 1000);
+    });
+
+    $("#services-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#services").offset().top - 64
+        }, 1000);
+    });
+
+    $("#eyewear-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#eyewear").offset().top - 64
+        }, 1000);
+    });
+
+    $("#forms-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#forms").offset().top - 64
+        }, 1000);
+    });
+
+    $("#contact-link").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top - 64
+        }, 1000);
+    });
+
+    $("#contact-us").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top - 64
+        }, 1000);
+    });
 });
